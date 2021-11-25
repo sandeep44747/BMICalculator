87f28b20c27ac223cdf2f79d66ade83d10406878
sandeep <reychoudhary07@gmail.com>
2021-11-25 19:44:06 +0530

    fav_movie task complete

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -1,6 +1,15 @@
 const movies = [];
 
 function favouriteMovie(operation, movie) {
+    if (operation=="add"){
+        movies.push(movie)
+        return movies
+    }
+    else{
+        movies.pop()
+        return movies
+    }
+
     // Write your code here
 }
 
